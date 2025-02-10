e3d723e8d9f3303d890fc2d432bc7a815b08b1fa
Sachin Sharma <code.dev.sachin@gmail.com>
2025-02-10 11:58:59 +0530

    Initial Commit

---
+++ Chronicle/ChronicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicleApp.swift
+//  Chronicle
+//
+//  Created by Sachin Sharma on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ChronicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
